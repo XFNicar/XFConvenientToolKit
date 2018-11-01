24f36b1aa4473e213ba4466685454bf2e35b1738
Fei.Xie <xuejingwen25@163.com>
2018-11-01 15:46:18 +0800

    Initial Commit

---
+++ XFConvenientToolKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XFConvenientToolKit
+//
+//  Created by YanYi on 2018/11/1.
+//  Copyright © 2018 YanYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XFConvenientToolKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XFConvenientToolKit
+//
+//  Created by YanYi on 2018/11/1.
+//  Copyright © 2018 YanYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
